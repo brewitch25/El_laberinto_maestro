7c08b9260fa4a27876dddf96b460647f8033ca90
Vanina Maldonado <vanimaldo.vm@gmail.com>
2026-04-16 13:04:38 -0300

    Se actualizo el uso de la libreria para generar aleatoriedad, ahora se usa la libreria <cstdlib>, se actualizo la constante CAMINO, solo va a mostrar un espacio vacio, se declaran primero las funciones a utilizar en el main y luego se muestran las funciones al final del main (para mejorar el orden y entendimiento), Se inicializa el generador de numeros aleatorios (srand). Se ajustan los valores de ancho y alto para sean siempre impares (para tener paredes en los caminos)

--- escapatoria.cpp
+++ escapatoria.cpp
@@ -2,33 +2,40 @@
 #include <vector>                // Se usa para generar la matriz del laberinto
 #include <ctime>                 // Se usa para generar los numeros aleatorios
 #include <chrono>                // Para medir el tiempo de ejecucion
-#include <stdlib.h>              // Para generar aleatoriedad
+#include <cstdlib>               // Para generar aleatoriedad
 using namespace std;             // Permite escribir de forma legible y facil de entender
 
 // Definimos las constantes para los elementos del laberinto
 const char SALIDA = 'S';
-const char MURO = '|';
+const char MURO = '#';
 const char ENTRADA = 'E';
-const char CAMINO = '.';
+const char CAMINO = ' ';
 const char RUTA = 'X';                          // Marca el camino resulto
 
+// Indice de todas las funciones a usar en main
+void generar_camino_desde(int fila, int col, int alto, int ancho, vector<vector<char>>& matriz);
+bool es_posicion_valida(int fila, int col, int alto, int ancho);
+
 int main() 
-{
+{    
+    // Inicializa los numeros aleatorios
+    srand(time(NULL));
+    
     // Inicializamos y declaramos las variables
     int ancho, alto;
-    
-    // Vamos a solicitar al usuario los siguientes valor (ancho y alto)
-    // Valor ancho (numero de filas)
+    // Solicitar al usuario valor ancho (numero de filas)
     cout <<"Ingrese el valor para el ancho del laberinto: "; cin >> ancho;
-    // Valor alto(numero de columnas)
+    // Solicitar al usuario valor alto(numero de columnas)
     cout <<"Ingrese el valor para el alto del laberinto: "; cin >> alto;
 
-    // Inicializo el laberinto
+    // Ajustamos los valores a impares para asegurar paredes entre los caminos
+    if (ancho % 2 == 0) ancho++;            // Se suma 1
+    if (alto % 2 == 0) alto++;              // Se suma 1
+
+    // Inicializo el laberinto lleno de MUROS
     vector<vector<char>> matriz_laberinto = vector<vector<char>>(alto, vector<char>(ancho, MURO));
 
-    // Output de prueba de visualizacion de inicializacion de laberinto
-    //cout <<"Laberinto creado de " << ancho << "x" << alto << endl;
-
+    
     return 0;
 }
 
